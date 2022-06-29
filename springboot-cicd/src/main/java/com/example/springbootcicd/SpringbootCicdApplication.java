c7f216313433edaf95428c0cdc8ce9dc5ccef63a
Rhea Mahuli <rhea.mahuli@gmail.com>
2022-06-29 09:42:49 -0400

    cicd springboot

---
+++ springboot-cicd/src/main/java/com/example/springbootcicd/SpringbootCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdApplication.class, args);
+	}
+
+}
